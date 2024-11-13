14236cb4a83eda852790b3cd8924a2a4fd548358
Admin <Admin@DESKTOP-UGP0C2G>
2024-11-13 21:47:56 +0200

    Добавьте файлы проекта.

---
+++ 324llfsadlf/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Func<double, double> function = x => Math.Pow(x, 2) - Math.Sin(Math.PI * x);
+        double a = 0.1;
+        double b = 2.9;
+        double h = 0.2;
+        Console.WriteLine("Таблица значений функции:");
+        for (double x = a; x <= b; x += h)
+        {
+            double y = function(x);
+            Console.WriteLine($"x = {x:F1}, y = {y:F4}");
+        }
+    }
+}
